1037285c2fc5112bbbbc5945ae0695cec9fce7eb
Yuki Tanaka <yukis5772@yahoo.co.jp>
2021-05-15 22:01:39 +0900

    first commit

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string name;
+  int tosi;
+
+  cout << "hello,I'm computar." << endl;
+  cout << "please input your name." << endl;
+  cin >> name;
+  cout << name << ",nice to meet you." << endl;
+  cout << "by the way," << name << ",how old are you?" << endl;
+  cin >> tosi;
+}
